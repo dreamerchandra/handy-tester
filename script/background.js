b4456b35cb596b67e1d9aa5c9b5361385b099795
dreamerchandra <17010408+dreamerchandra@users.noreply.github.com>
2019-06-01 16:57:06 +0530

    background messenger code refactor

--- script/background.js
+++ script/background.js
@@ -192,114 +192,123 @@ async function startReproducing(request) {
 }
 
 const messageListener = (request, sender, sendResponse) => {
-  switch (request.query) {
-    case 'getScenarioNames':
-      StorageUtils.getItem('Scenario names').then((data) => {
+  async function _messageListener() {
+    switch (request.query) {
+      case 'getScenarioNames':
+        const data = await StorageUtils.getItem('Scenario names');
         console.log('scenario names', data);
         sendResponse(data);
-      });
-      break;
-    case 'isScenarioExist':
-      isScenarioExist(request).then((isExist) => {
+        break;
+      case 'isScenarioExist':
+        const isExist = await isScenarioExist(request);
         sendResponse(isExist);
-      });
-      break;
-    case 'record':
-      console.log('recording ', request);
-      recordEvents(request).then((isSuccess) => {
-        if (isSuccess) {
-          console.log('recording successful', request);
-          sendResponse('Done');
-        } else {
-          console.log('recording failed', request);
+        break;
+      case 'record':
+        try {
+          console.log('recording ', request);
+          const isSuccess = await recordEvents(request);
+          if (isSuccess) {
+            console.log('recording successful', request);
+            sendResponse('Done');
+          } else {
+            console.log('recording failed', request);
+            sendResponse('Failed');
+          }
+        } catch (error) {
+          console.log('recording failed for', request, 'with error', error);
           sendResponse('Failed');
         }
-      }).catch((error) => {
-        console.log('recording failed for', request, 'with error', error);
-        sendResponse('Failed');
-      });
-      break;
-    case 'reproduce':
-      if (request.queryType == 'getEventIds') { // returns event ids associated with scenario
-        console.log('getting event ids associated with scenario', request);
-        getEventIdsForScenario(request).then((eventIds) => {
-          console.log('event ids are ', eventIds);
-          sendResponse(eventIds);
-        }).catch((error) => {
-          console.log('getting event ids failed', error);
-          sendResponse('Failed');
-        });
-      } else if (request.queryType == 'getEventData') { // return individual events
-        console.log('getting event', request);
-        getEventData(request).then((data) => {
-          console.log('event data is', data);
-          sendResponse(data);
-        }).catch((error) => {
-          console.log('getting event data failed', error);
-          sendResponse('Failed');
-        });
-      }
-      break;
-    case 'startRecording':
-      startRecording(request).then((canStart) => {
-        console.log('recording can start:', canStart, ' for request ', request);
-        sendResponse(canStart);
-      }).catch((error) => {
-        console.log('recording can start:', error);
-        sendResponse('Failed');
-      });
-      break;
-    case 'currentAction':
-      StorageUtils.getItem(request.queryType).then((scenarioName) => {
-        console.log(request.queryType, ' ', scenarioName ? scenarioName : 'nothing');
-        sendResponse(scenarioName);
-      }).catch((error) => {
-        console.log('currentlyRecording failed to set', error);
-        sendResponse('Failed');
-      });
-      break;
-    case 'stopAction':
-      ChromeUtils.detachListenerForTabStatusChange();
-      StorageUtils.removeItem(request.queryType).then((removed) => {
-        console.log(request.queryType,' removed ', removed ? 'success' : 'failed');
-        sendResponse(removed ? true : false);
-      }).catch((error) => {
-        console.log('stop recording failed to stop:', error);
-        sendResponse('Failed');
-      });
-      break;
-    case 'activeTabId':
-      ChromeUtils.getActiveTabInfo('id').then((tabId) => {
-        console.log("active tab id is ", tabId);
-        sendResponse(tabId);
-      }).catch((error) => {
-        console.log('stop recording failed to stop:', error);
-        sendResponse('Failed');
-      });
-      break;
-    case 'startReproduce':
-      startReproducing(request).then((canStart) => {
-        console.log('reproducing can start:', canStart, ' for request ', request);
-        sendResponse(canStart);
-      }).catch((error) => {
-        console.log('recording can start:', error);
-        sendResponse('Failed');
-      });
-      break;
-    case 'unload':
-      console.log('reproduce going to unload', request);
-      if (request.queryType === 'reproduce') {
-        ChromeUtils.getActiveTabInfo('id').then((activeTabId) => {
-          if (activeTabId) {
-            ChromeUtils.listenForTabStatusChange
-              (activeTabId, 'complete', triggerReproduceOnReload, request.resumeEventAt);
+        break;
+      case 'reproduce':
+        if (request.queryType == 'getEventIds') { // returns event ids associated with scenario
+          try {
+            console.log('getting event ids associated with scenario', request);
+            const eventIds = await getEventIdsForScenario(request);
+            console.log('event ids are ', eventIds);
+            sendResponse(eventIds);
+          } catch (error) {
+            console.log('getting event ids failed', error);
+            sendResponse('Failed');
           }
-        }).catch((error) => {
-          console.log('unload failed:', error);
+        } else if (request.queryType == 'getEventData') { // return individual events
+          try {
+            console.log('getting event', request);
+            const data = await getEventData(request)
+            console.log('event data is', data);
+            sendResponse(data);
+          } catch (error) {
+            console.log('getting event data failed', error);
+            sendResponse('Failed');
+          }
+        }
+        break;
+      case 'startRecording':
+        try {
+          const canStart = await startRecording(request);
+          console.log('recording can start:', canStart, ' for request ', request);
+          sendResponse(canStart);
+        } catch (error) {
+          console.log('recording can start:', error);
           sendResponse('Failed');
-        });;
-      }
+        }
+        break;
+      case 'currentAction':
+        try {
+          const scenarioName = await StorageUtils.getItem(request.queryType);
+          console.log(request.queryType, ' ', scenarioName ? scenarioName : 'nothing');
+          sendResponse(scenarioName);
+        } catch (error) {
+          console.log('currentlyRecording failed to set', error);
+          sendResponse('Failed');
+        }
+        break;
+      case 'stopAction':
+        try {
+          ChromeUtils.detachListenerForTabStatusChange();
+          const removed = await StorageUtils.removeItem(request.queryType);
+          console.log(request.queryType, ' removed ', removed ? 'success' : 'failed');
+          sendResponse(removed ? true : false);
+        } catch (error) {
+          console.log('stop recording failed to stop:', error);
+          sendResponse('Failed');
+        }
+        break;
+      case 'activeTabId':
+        try {
+          const tabId = await ChromeUtils.getActiveTabInfo('id');
+          console.log("active tab id is ", tabId);
+          sendResponse(tabId);
+        } catch (error) {
+          console.log('stop recording failed to stop:', error);
+          sendResponse('Failed');
+        }
+        break;
+      case 'startReproduce':
+        try {
+          const canStart = await startReproducing(request);
+          console.log('reproducing can start:', canStart, ' for request ', request);
+          sendResponse(canStart);
+        } catch (error) {
+          console.log('recording can start:', error);
+          sendResponse('Failed');
+        }
+        break;
+      case 'unload':
+        console.log('reproduce going to unload', request);
+        if (request.queryType === 'reproduce') {
+          try {
+            const activeTabId = await ChromeUtils.getActiveTabInfo('id');
+            if (activeTabId) {
+              ChromeUtils.listenForTabStatusChange(activeTabId, 'complete', triggerReproduceOnReload, request.resumeEventAt);
+            }
+          } catch (error) {
+            console.log('unload failed:', error);
+            sendResponse('Failed');
+          }
+        }
+    }
   }
+  _messageListener();
   return true;
 }
 
